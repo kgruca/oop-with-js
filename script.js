8759149b43d4322bc51f2581772049f1b3ef4c2c
Krzysztof Gruca <kgruca21@gmail.com>
2022-10-30 21:39:08 -0700

    constructor prototypes

--- script.js
+++ script.js
@@ -24,3 +24,38 @@ console.log(krzysztof);
 
 const sveta = new Person('Svitlana', 1988);
 // can make as many objects as we want from the constructor function
+
+
+// NEW SECTION
+// Prototypes
+
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear);
+};
+// now, the calcAge function is a method for the Person constructor function
+
+krzysztof.calcAge();
+// result is 48
+// any object (instance) has access the methods and properties of its prototype
+
+console.log(krzysztof.__proto__);
+// logs the prototype itself
+console.log(krzysztof.__proto__ === Person.prototype);
+// logs true
+console.log(Person.prototype.isPrototypeOf(krzysztof));
+// logs true
+console.log(Person.prototype.isPrototypeOf(sveta));
+// logs true 
+console.log(Person.prototype.isPrototypeOf(Person));
+// logs false
+
+// prototype of linked objects
+
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(krzysztof, sveta);
+// logs the object information; species and calcAge are listed under the prototype
+
+console.log(krzysztof.species, sveta.species);
+// logs Homo Sapiens Homo Sapiens
+
